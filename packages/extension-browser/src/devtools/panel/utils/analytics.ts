8fbde328068c8374764b96306933b78b1a73dd84
Tony Ross <antross@gmail.com>
2019-03-04 10:50:43 -0600

    New: Report pass/fail status for hints

--- packages/extension-browser/src/devtools/panel/utils/analytics.ts
+++ packages/extension-browser/src/devtools/panel/utils/analytics.ts
@@ -1,5 +1,7 @@
 import { AppInsights } from 'applicationinsights-js';
 
+import { HintResults, Results } from '../../../shared/types';
+
 /** Called to initialize the underlying analytics library. */
 export const setup = () => {
     AppInsights.downloadAndSetup!({ instrumentationKey: '8ef2b55b-2ce9-4c33-a09a-2c3ef605c97d' });
@@ -23,4 +25,30 @@ export const trackShow = () => {
 /** Called when analysis was started by the user. */
 export const trackStart = () => {
     AppInsights.trackEvent('f12-start');
+};
+
+const trackResults = (results: Results): void => {
+    const hints = results.categories.reduce((list, category) => {
+        return [...list, ...category.hints];
+    }, [] as HintResults[]);
+
+    const passed = hints.filter((hint) => {
+        return hint.problems.length === 0;
+    }).map((hint) => {
+        return hint.id;
+    });
+
+    const failed = hints.filter((hint) => {
+        return hint.problems.length > 0;
+    }).map((hint) => {
+        return hint.id;
+    });
+
+    // TODO
+    AppInsights.trackEvent('f12-results', undefined, {
+        'f12-fixed-hint-axe': 0, // Date.now() - firstSeen
+        'f12-fixed-hint-button-type': 600000,
+        'f12-hint-axe': 12, // hint.problems.length
+        'f12-hint-button-type': 4
+    });
 };
