83f7d40e786ef80f765b5dc3359ae3a14f5f7fe2
Tony Ross <antross@gmail.com>
2020-02-13 16:46:39 -0600

    Chore: Bundle English strings for now

--- packages/utils-worker/src/shims/get-message.ts
+++ packages/utils-worker/src/shims/get-message.ts
@@ -1,4 +1,5 @@
 import camelCase = require('lodash/camelCase');
+import messages = require('../../dist/bundle/_locales/en/messages.json');
 
 type GetMessage = typeof import('@hint/utils-i18n').getMessage;
 
@@ -13,7 +14,12 @@ export const getMessage: GetMessage = (key, path, options?) => {
     const packageName = pathParts[pathParts.length - 3];
     const prefix = camelCase(packageName);
     const substitutions = options && options.substitutions;
+    const name = `${prefix}_${key}`;
 
-    // TODO: Get actual localized message.
-    return `${prefix}_${key} ${substitutions}`;
+    if (!(messages as any)[name]) {
+        console.log('Name', name);
+        console.log('Message', messages);
+    }
+
+    return (messages as any)[name].message + (substitutions || '');
 };
